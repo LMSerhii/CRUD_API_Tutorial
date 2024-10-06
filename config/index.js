24a1102fb35555183f5cbd219115400a0638d741
LMSerhii <117682588+LMSerhii@users.noreply.github.com>
2024-10-06 05:51:04 +0300

    santry error added

--- config/index.js
+++ config/index.js
@@ -4,6 +4,7 @@ dotenv.config();
 
 const CONFIG = {
   PORT: process.env.PORT,
+  SENTRY_DSN: process.env.SENTRY_DSN,
 };
 
 export default CONFIG;
--- index.js
+++ index.js
@@ -1,8 +1,18 @@
+import * as Sentry from '@sentry/node';
+import { nodeProfilingIntegration } from '@sentry/profiling-node';
 import cors from 'cors';
 import express from 'express';
 import morgan from 'morgan';
-import routes from './src/routes/index.js';
 import CONFIG from './config/index.js';
+import routes from './src/routes/index.js';
+
+Sentry.init({
+  dsn: CONFIG.SENTRY_DSN,
+  integrations: [nodeProfilingIntegration()],
+  tracesSampleRate: 1.0,
+
+  profilesSampleRate: 1.0,
+});
 
 const app = express();
 
@@ -11,6 +21,8 @@ app.use(cors());
 app.use(express.json());
 
 routes(app);
+
+Sentry.setupExpressErrorHandler(app);
 
 const port = CONFIG.PORT || 3001;
 
