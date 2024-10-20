2556d7cce049b57a1dfb765594d24c402db02ecc
LMSerhii <117682588+LMSerhii@users.noreply.github.com>
2024-10-20 12:06:58 +0300

    update config file

--- src/config/index.ts
+++ src/config/index.ts
@@ -7,47 +7,19 @@ dotenv.config({
 
 interface Config {
   PORT: string;
-  DB_HOST: string;
-  DB_NAME: string;
-  DB_PSW: string;
-  DB_PORT: string;
   DATABASE_URL: string;
-  SENTRY_PROJECT_ID: string;
-  SENTRY_ORGANIZATION_ID: string;
-  SENTRY_KEY: string;
   SENTRY_DSN: string;
 }
 
 function createConfig(): Config {
-  const { PORT, DB_HOST, DB_NAME, DB_PSW, DB_PORT, SENTRY_PROJECT_ID, SENTRY_ORGANIZATION_ID, SENTRY_KEY } =
-    process.env;
+  const { PORT, DATABASE_URL, SENTRY_DSN } = process.env;
 
-  if (
-    !PORT ||
-    !DB_HOST ||
-    !DB_NAME ||
-    !DB_PSW ||
-    !DB_PORT ||
-    !SENTRY_PROJECT_ID ||
-    !SENTRY_ORGANIZATION_ID ||
-    !SENTRY_KEY
-  ) {
+  if (!PORT || !SENTRY_DSN || !DATABASE_URL) {
     throw new Error('Missing one or more configuration environment variables');
   }
 
-  const DATABASE_URL = `postgresql://${DB_NAME}:${DB_PSW}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
-
-  const SENTRY_DSN = `https://${SENTRY_KEY}@o${SENTRY_ORGANIZATION_ID}.ingest.de.sentry.io/${SENTRY_PROJECT_ID}`;
-
   return {
     PORT,
-    DB_HOST,
-    DB_NAME,
-    DB_PSW,
-    DB_PORT,
-    SENTRY_PROJECT_ID,
-    SENTRY_ORGANIZATION_ID,
-    SENTRY_KEY,
     DATABASE_URL,
     SENTRY_DSN,
   };
